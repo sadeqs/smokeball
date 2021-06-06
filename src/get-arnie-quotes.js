35bfe2d269883ebc54a5f75b51d9d6a765335b21
Sadegh Salehi <SadeghSalehi@GMail.com>
2021-06-06 20:31:44 +1000

    implement logic for function getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,25 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(
+    urls.map( async url => {
+      try{
+        const result = await httpGet(url);
+        const message = JSON.parse(result.body).message
+        if(result.status === 200) {
+          return {'Arnie Quote': message};
+        }
+        throw new Error(message);
+        /** 
+         * to catch every probable error when real httpGet is implemented plus errors returned from server
+         * There wasn't any requirement to seperate different types of error, so, I implemented all of them as 'FAILURE'
+         * */
+      } catch (e){
+        return {'FAILURE': e.message};
+      }
+    })
+  )
+  return results;
 };
 
 module.exports = {
